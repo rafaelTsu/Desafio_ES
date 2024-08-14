68095ebf3304f8ac4a8714b0d5427a266bbd87b4
Rafael Tsutomu <rafael.tsutomu@ufms.br>
2024-08-14 01:38:41 -0400

    feat: faz a conexão com firebase authentication

---
+++ frontend/src/contexts/AuthContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { auth } from '../config/firebaseConfig';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
